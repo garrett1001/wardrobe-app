c35594d3a21fc3e4fb3ce9a0fd29594768b659f5
garrett1001 <garrettwelton@gmail.com>
2022-04-11 01:02:18 -0700

    Updated Userprofile and NewUserForm

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -4,4 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserProfile(models.Model):
    user = models.OneToOneField(User, on_delete=models.CASCADE)
+   avatar = models.ImageField(upload_to='images/avatars')
+   biography = models.CharField(max_length=200)
    private = models.BooleanField(default=False)
